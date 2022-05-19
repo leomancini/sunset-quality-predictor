0c516991a6b8edb06ac959be8c50182ab6f6aee7
Leo Mancini <mancini@leo.gd>
2022-05-18 20:50:21 -0400

    Adding formatDate

--- model/functions.js
+++ model/functions.js
@@ -19,3 +19,14 @@ export function cleanDate(dateYYYMMDD) {
         ISOString
     };
 }
+
+export function formatDate(date) {
+    let yyyy = date.getFullYear().toString();
+    let mm = (date.getMonth()+1).toString();
+    let dd  = date.getDate().toString();
+
+    let mmChars = mm.split('');
+    let ddChars = dd.split('');
+
+    return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+}
