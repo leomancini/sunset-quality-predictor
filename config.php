e24e2c0273ef7458018507d6b16468a3b71808ad
Leo Mancini <mancini@leo.gd>
2022-11-05 12:14:22 -0400

    Removing lat/lng from config

--- config.php
+++ config.php
@@ -4,10 +4,6 @@
     $CONFIG = [
         'SERVER' => 'http://192.168.1.20',
         'IMG_PATH' => 'nest-cam-timelapse/images/SKYLINE',
-        'LOCATION' => [
-            'LAT' => '40.730610',
-            'LNG' => '-73.935242'
-        ],
         'TIMEZONE' => 'America/New_York',
         'FIRST_DATE' => '2021-12-16'
     ];
